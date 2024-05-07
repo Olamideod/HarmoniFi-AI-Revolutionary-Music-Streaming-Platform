47258fe326bc43fc281410e7ab789eb9237c67d5
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2024-05-07 19:53:50 +0100

    updates

--- frontend/components/Search.jsx
+++ frontend/components/Search.jsx
@@ -28,16 +28,16 @@ const Search = ({ setView, setGlobalPlaylistId, setGlobalCurrentSongId, setGloba
     }, [inputRef])
 
     return (
-        <div className='flex-grow h-screen'>
+        <div className=' flex-grow h-screen'>
             <header className='text-white sticky top-0 h-20 z-10 text-4xl flex items-center px-8'>
                 <MagnifyingGlassIcon className='absolute top-7 left-10 h-6 w-6 text-neutral-800' />
                 <input value={inputValue} onChange={async (e) => {
                     setInputValue(e.target.value)
                     await updateSearchResults(e.target.value)
-                }} ref={inputRef} className='rounded-full bg-white w-96 pl-12 text-neutral-900 text-base py-2 font-normal outline-0' />
+                }} ref={inputRef} className='rounded-full bg-white w-100 pl-12 text-neutral-900 text-base py-2 font-normal outline-0' />
             </header>
-            <div onClick={() => signOut()} className='absolute z-20 top-5 right-8 flex items-center bg-black bg-opacity-70 text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
-                <img className='rounded-full w-7 h-7' src={session?.user.image} alt="profile pic" />
+            <div onClick={() => signOut()} className='absolute z-20 top-5 right-8 flex items-center bg-white bg-opacity-70 text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+                <img className='rounded-full w-0 h-7' src={session?.user.image} alt="profile pic" />
                 <p className='text-sm'>Logout</p>
                 <ChevronDownIcon className='h-5 w-5' />
             </div>
